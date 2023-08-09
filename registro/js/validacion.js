586baa00db99b66ef06ec18bd06d30cfd2355e1d
Alejandro Fandiño <138483103+afandino21@users.noreply.github.com>
2023-08-09 12:10:09 -0300

    Ale (#3)
    * hice un compendio de lo que vi hasta ahora en el main y en la rama de ema
    
    las mezcle para que borre los datos si se completa todo correctamente
    
    * arreglo algunos errores, quedo funcional
    
    muestra los carteles con texto cuando hay error, muestra cartel de exito y borra los datos cuando hay exito

--- registro/js/validacion.js
+++ registro/js/validacion.js
@@ -10,15 +10,6 @@ function showAlertError(message) {
     setTimeout(() => {
         alertDanger.classList.remove("show");
     }, 2000);
-}
-
-function clearFormFields() {
-    nombre.value = "";
-    apellido.value = "";
-    email.value = "";
-    password1.value = "";
-    password2.value = "";
-    terminos.checked = false;
 
 }
 
@@ -29,26 +20,34 @@ function clearFormFields() {
     password1.value = "";
     password2.value = "";
     terminos.checked = false;
+
+
+}
+
+function validateForm() {
+    const nombre = document.getElementById("nombre").value;
+    const apellido = document.getElementById("apellido").value;
+    const email = document.getElementById("email").value;
+    const password1 = document.getElementById("password1").value;
+    const password2 = document.getElementById("password2").value;
+    const terminos = document.getElementById("terminos").checked;
+
+    if (!nombre || !apellido || !email || !password1 || !password2) {
+        showAlertError("Por favor, complete todos los campos.");
+        return false;
+    }
+
+    if (password1.length < 6) {
+        showAlertError("La contraseña debe tener al menos 6 caracteres.");
+        return false;
+    }
+
+    if (password1 !== password2) {
+        showAlertError("Las contraseñas no coinciden.");
+        return false;
+    }
+
   }
-
-document.addEventListener("DOMContentLoaded", function() {
-    const regBtn = document.getElementById("regBtn");
-    const nombre = document.getElementById("nombre");
-    const password1 = document.getElementById("password1");
-    const password2 = document.getElementById("password2");
-    const terminos = document.getElementById("terminos");
-
-    regBtn.addEventListener("click", function() {
-      if (nombre.value === "" || apellido.value === "" || email.value === "" ||
-          password1.value === "" || password2.value === "" || !terminos.checked) {
-        showAlertError();
-        return;
-      }
-
-      if (password1.value.length < 6 || password1.value !== password2.value) {
-        showAlertError();
-        return;
-      }
 
     if (!terminos) {
         showAlertError("Debe aceptar los términos y condiciones del servicio.");
@@ -63,5 +62,7 @@ document.getElementById("regBtn").addEventListener("click", function () {
     if (validateForm()) {
         clearFormFields();
     }
+
 });
+
 
